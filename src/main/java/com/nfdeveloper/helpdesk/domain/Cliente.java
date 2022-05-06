dc64b2a8942f3e1fc96d3e0995aa2a4eadd39ec6
nfdeveloper <jnvlfilho@hotmail.com>
2022-05-05 22:22:19 -0300

    Criacao de classes de Dominio

---
+++ src/main/java/com/nfdeveloper/helpdesk/domain/Cliente.java
@@ -0,0 +1,28 @@
+package com.nfdeveloper.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente extends Pessoa{
+
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email) {
+		super(id, nome, cpf, email);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
+	
+}
