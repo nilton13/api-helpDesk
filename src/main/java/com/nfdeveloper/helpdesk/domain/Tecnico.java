a038bb654a4a560960cb5bd3f86ec4cdd3f54988
nfdeveloper <jnvlfilho@hotmail.com>
2022-05-05 22:35:13 -0300

    Inserindo anotacoes nas classes de Dominio

--- src/main/java/com/nfdeveloper/helpdesk/domain/Tecnico.java
+++ src/main/java/com/nfdeveloper/helpdesk/domain/Tecnico.java
@@ -3,18 +3,28 @@ package com.nfdeveloper.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa{
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
+import com.nfdeveloper.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa{
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email) {
 		super(id, nome, cpf, email);
+		addPerfil(Perfil.CLIENTE);
 	}
-
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
