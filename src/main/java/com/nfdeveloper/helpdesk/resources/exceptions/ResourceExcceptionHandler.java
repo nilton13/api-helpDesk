aae3857a5afb41d2efdfe0ff55a13ce785ed137a
nfdeveloper <jnvlfilho@hotmail.com>
2022-05-07 10:42:35 -0300

    Criando tratamento de erros customizado

---
+++ src/main/java/com/nfdeveloper/helpdesk/resources/exceptions/ResourceExcceptionHandler.java
@@ -0,0 +1,27 @@
+package com.nfdeveloper.helpdesk.resources.exceptions;
+
+import com.nfdeveloper.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResourceExcceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
+                                                                 HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                "Object not Found",
+                ex.getMessage(),
+                request.getRequestURI()
+                );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
