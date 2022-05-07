4461800e8eaeaef266a792a54bba09f91a17dbe8
nfdeveloper <jnvlfilho@hotmail.com>
2022-05-07 10:29:03 -0300

    Criando rota de FinById e Utilizando o padrao DTO

---
+++ src/main/java/com/nfdeveloper/helpdesk/resources/TecnicoResources.java
@@ -0,0 +1,25 @@
+package com.nfdeveloper.helpdesk.resources;
+
+import com.nfdeveloper.helpdesk.domain.Tecnico;
+import com.nfdeveloper.helpdesk.domain.dtos.TecnicoDTO;
+import com.nfdeveloper.helpdesk.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResources {
+
+    @Autowired
+    private TecnicoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
+        Tecnico obj = service.findById(id);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
+    }
+}
