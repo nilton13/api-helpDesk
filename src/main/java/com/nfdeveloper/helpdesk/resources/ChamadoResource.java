110a1f783816288eff6cc46acfada1b075c36d5e
nfdeveloper <jnvlfilho@hotmail.com>
2022-05-07 19:55:57 -0300

    Criando crud de Chamados

---
+++ src/main/java/com/nfdeveloper/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,52 @@
+package com.nfdeveloper.helpdesk.resources;
+
+import com.nfdeveloper.helpdesk.domain.Chamado;
+import com.nfdeveloper.helpdesk.domain.dtos.ChamadoDTO;
+import com.nfdeveloper.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+        Chamado obj = service.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll(){
+        List<Chamado> list = service.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+        Chamado obj = service.create(objDTO);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id,@Valid @RequestBody ChamadoDTO objDTO){
+        Chamado newObj = service.update(id, objDTO);
+        return ResponseEntity.ok().body(new ChamadoDTO(newObj));
+    }
+}
---
+++ src/main/java/com/nfdeveloper/helpdesk/services/ChamadoService.java
@@ -0,0 +1,72 @@
+package com.nfdeveloper.helpdesk.services;
+
+
+import com.nfdeveloper.helpdesk.domain.Chamado;
+import com.nfdeveloper.helpdesk.domain.Cliente;
+import com.nfdeveloper.helpdesk.domain.Tecnico;
+import com.nfdeveloper.helpdesk.domain.dtos.ChamadoDTO;
+import com.nfdeveloper.helpdesk.domain.enums.Prioridade;
+import com.nfdeveloper.helpdesk.domain.enums.Status;
+import com.nfdeveloper.helpdesk.repositories.ChamadoRepository;
+import com.nfdeveloper.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+    @Autowired
+    private ClienteService clienteService;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado"));
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO objDTO) {
+        return repository.save(newChamado(objDTO));
+    }
+
+    public Chamado update(Integer id, ChamadoDTO objDTO) {
+        objDTO.setId(id);
+        Chamado oldObj = findById(id);
+        oldObj = newChamado(objDTO);
+        return repository.save(oldObj);
+    }
+
+    private Chamado newChamado(ChamadoDTO obj){
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(obj.getId() != null){
+            chamado.setId(obj.getId());
+        }
+
+        if(obj.getStatus().equals(2)){
+            chamado.setDataFechamento(LocalDate.now());
+        }
+
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
+    }
+
+
+}
