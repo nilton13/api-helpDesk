8097961d8381d0ddfaf79eb304f8efa31bd410e6
nfdeveloper <jnvlfilho@hotmail.com>
2022-05-07 19:11:38 -0300

    Deletando tecnicos

--- src/main/java/com/nfdeveloper/helpdesk/resources/TecnicoResources.java
+++ src/main/java/com/nfdeveloper/helpdesk/resources/TecnicoResources.java
@@ -53,4 +53,10 @@ public class TecnicoResources {
         Tecnico obj = service.update(id, objDTO);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/nfdeveloper/helpdesk/services/TecnicoService.java
+++ src/main/java/com/nfdeveloper/helpdesk/services/TecnicoService.java
@@ -45,6 +45,14 @@ public class TecnicoService {
         return repository.save(oldObj);
     }
 
+    public void delete(Integer id) {
+        Tecnico obj = findById(id);
+        if(obj.getChamados().size() > 0){
+            throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado");
+        }
+        repository.delete(obj);
+    }
+
     private void validaPorCpfeEmail(TecnicoDTO objDTO) {
         Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
         if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
@@ -56,6 +64,5 @@ public class TecnicoService {
             throw new DataIntegrityViolationException("E-mail já cadastrado no sistema");
         }
     }
-
 
 }
